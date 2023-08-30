dff7bdc6439488254698115ef0e1f905b1a458c7
Erivelton Campos <ercampos@id.uff.br>
2023-08-30 13:18:42 -0300

    chore: created component to header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import style from './Header.module.css'
+
+export function HeaderSite(){
+    return(
+        <div>
+            <header>
+
+            </header>
+            <body>
+                
+            </body>
+        </div>
+    )
+}
