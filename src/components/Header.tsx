5189ebe449f0e1274fca834ead2c280cff87c07d
Erivelton Campos <ercampos@id.uff.br>
2023-08-30 14:11:34 -0300

    fix: background photo to display larger than 1440p

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import style from './Header.module.css'
+import photoBackground from '../assets/foto_de_fundo.svg'
 import { FacebookLogo } from 'phosphor-react';
 import { InstagramLogo } from 'phosphor-react';
 import { Soda } from './SodaPicture';
@@ -6,11 +7,16 @@ import { Soda } from './SodaPicture';
 export function HeaderSite(){
     return(
         <div className={style.headerConter}>
+            <img src={photoBackground} alt=""/>
             <header className={style.head}>
                 <h1>CONECTA.</h1>
                 <div className={style.socialMediaLogo}>
-                    <FacebookLogo/>
-                    <InstagramLogo/>
+                    <a href="">
+                        <FacebookLogo size={22}/>
+                    </a>
+                    <a href="">
+                        <InstagramLogo size={22}/>
+                    </a>
                 </div>
             </header>
             <body className={style.bodySite}>
