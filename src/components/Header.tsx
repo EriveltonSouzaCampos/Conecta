4e111ce57383962c5bd81dc11e77b94606776caa
Erivelton Campos <ercampos@id.uff.br>
2023-08-30 13:28:56 -0300

    feat: finished header secction

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,19 @@
 import style from './Header.module.css'
+import { FacebookLogo } from 'phosphor-react';
+import { InstagramLogo } from 'phosphor-react';
+import { Soda } from './SodaPicture';
 
 export function HeaderSite(){
     return(
-        <div>
-            <header>
-
+        <div className={style.headerConter}>
+            <header className={style.head}>
+                <h1>CONECTA.</h1>
+                <div className={style.socialMediaLogo}>
+                    <FacebookLogo/>
+                    <InstagramLogo/>
+                </div>
             </header>
-            <body>
+            <body className={style.bodySite}>
                 
             </body>
         </div>
