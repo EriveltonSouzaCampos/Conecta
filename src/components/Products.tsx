92d23f78a0f9e63eabed7ec6a92b11ec57b7b591
Erivelton Campos <ercampos@id.uff.br>
2023-08-30 22:27:44 -0300

    feat: created new component to products

---
+++ src/components/Products.tsx
@@ -0,0 +1,11 @@
+
+export function Prods(){
+    return(
+        <div>
+            <img src="" alt="Imagem do produto" />
+            <h1>Nome</h1>
+            <p>Desc</p>
+            <button><a href="">Aqui um CTA</a></button>
+        </div>
+    )
+}
