25e4d5bee3ae7fd2ca009ce0e37a2371592c55da
Erivelton Campos <ercampos@id.uff.br>
2023-08-30 22:28:11 -0300

    feat: created prods section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { HeaderSite } from "./components/Header"
 import style from './App.module.css'
+import { Prods } from "./components/Products"
 
 function App() {
 
   return (
    <div>
-      <HeaderSite/>
+      <header><HeaderSite/></header>
+      <div>
+          <h1>Produtos</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Volupta, reiciendis quo?</p>
+          <div>
+              <Prods/>
+          </div>
+      </div>
    </div>
   )
 }
