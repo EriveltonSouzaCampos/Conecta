34980edf7a57832202d948169dfeafef02e5dee8
Erivelton Campos <ercampos@id.uff.br>
2023-08-30 23:22:29 -0300

    feat: created interface to recive info of prods

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,10 +1,16 @@
+import style from './Products.module.css'
+interface prodsProps{
+    name: string;
+    img: string;
+    description: string;
+} 
 
-export function Prods(){
+export function Prods(props: prodsProps){
     return(
-        <div>
-            <img src="" alt="Imagem do produto" />
-            <h1>Nome</h1>
-            <p>Desc</p>
+        <div className={style.prodConter}>
+            <img src={props.img} alt="Imagem do produto" />
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
             <a href=""><button type="button">Aqui um CTA</button></a>
         </div>
     )
