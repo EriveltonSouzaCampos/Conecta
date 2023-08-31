46a211eb1bd377804d4a99ac8d2dfe4fbafce447
Erivelton Campos <ercampos@id.uff.br>
2023-08-31 13:10:24 -0300

    chore: created new div

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,11 +6,14 @@ interface prodsProps{
 } 
 
 export function Prods(props: prodsProps){
+    console.log(props.img)
     return(
         <div className={style.prodConter}>
-            <img src={props.img} alt="Imagem do produto" />
-            <h1>{props.name}</h1>
-            <p>{props.description}</p>
+            <div className={style.prodContent}>
+                <img src={props.img} alt="Imagem do produto" />
+                <h1>{props.name}</h1>
+                <p>{props.description}</p>
+            </div>
             <a href=""><button type="button">Aqui um CTA</button></a>
         </div>
     )
