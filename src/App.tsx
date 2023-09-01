eeec3a2282499688846b67fbfd885f2e16f56551
Erivelton Campos <ercampos@id.uff.br>
2023-09-01 13:08:39 -0300

    chore: changing name of products

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,24 @@ function App() {
     {
       id: 1,
       img: '/src/assets/latas.svg',
-      name: 'Lata de Cola',
-      description: 'lorem lorem lorem lorem e lorem'
+      name: 'Garrafa 350ml',
+      description: 'Lorem ipsum dolor sit amet'
     },
      {
        id: 2,
         img: '/src/assets/garrafa.svg',
-       name: 'Lata de Cola',
-       description: 'lorem lorem lorem lorem e lorem'
+       name: 'Garrafa 150ml',
+       description: 'Lorem ipsum dolor sit amet'
      },
   ]
   return (
    <div>
       <header><HeaderSite/></header>
-      <div>
-          <h1>Produtos</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Volupta, reiciendis quo?</p>
+      <div className={style.productConter}>
+          <span>
+            <h1>Produtos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Volupta, reiciendis quo?</p>
+          </span>
           <div className={style.product}>
               {
                 products.map((prod) => (
