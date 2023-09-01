3ebc7f94553bcd0084bcca599772683a62186221
Erivelton Campos <ercampos@id.uff.br>
2023-09-01 15:52:12 -0300

    feat: created component to structure slides

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import style from './Carousel.module.css';
+interface Slide {
+    text: string;
+    author: string;
+}
+interface TextCarouselProps {
+  slides: Slide[];
+}
+
+const Carousel = ({ slides }: TextCarouselProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToSlide = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className={style.carouselConter}>
+      <div className={style.slide}>{slides[currentIndex].text}</div>
+      <div className={style.author}>{slides[currentIndex].author}</div>
+      <div className={style.dotsConter}>
+        {slides.map((slide, index) => (
+          <span
+            key={index}
+            className={`${style.dot} ${index === currentIndex ? style.active : ''}`}
+            onClick={() => goToSlide(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
