b4cb9194bc6cc872dbac589229e04526d8db144c
Erivelton Campos <ericampos119@gmail.com>
2023-09-02 01:19:40 -0300

    feat: add span to put in column label and input

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,29 +1,40 @@
 import { Button } from "./Button";
+import style from './Forms.module.css'
 
 export function Forms() {
     const nothing = ' '
     return (
-      <div>
-        <label htmlFor="text">{nothing}</label>
-        <input type="text" id="text" />
-        <div>
-            <label htmlFor="tel">{nothing}</label>
-            <input type="tel" id="tel" />
-            <label htmlFor="email">{nothing}</label>
-            <input type="text" id="email" />
+    <div className={style.formConter}>
+        <span>
+            <label htmlFor="text">Nome</label>
+            <input type="text" id="text" />
+        </span>
+        <div className={style.telAndEmail}>
+            <span>
+                <label htmlFor="tel">Tel</label>
+                <input type="tel" id="tel" />
+            </span>
+            <span>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" />
+            </span>
         </div>
-        <div>
-        <label htmlFor="drop">{nothing}</label>
-            <select name="" id="drop">
-                <option value="1">oi</option>
-                <option value="2">oie</option>
-                <option value="3">oiee</option>
-                <option value="4">oioioi</option>
-            </select>
-            <label htmlFor="textArea">{nothing}</label>
-            <textarea id="textArea" cols={30} rows={10}></textarea>
+        <div className={style.dropAndArea}>
+            <span>
+                <label htmlFor="drop">Assunto</label>
+                <select id="drop">
+                    <option value="1">oi</option>
+                    <option value="2">oie</option>
+                    <option value="3">oiee</option>
+                    <option value="4">oioioi</option>
+                </select>
+            </span>
+            <span>
+                <label htmlFor="textArea">Mensagem</label>
+                <textarea id="textArea" cols={30} rows={10}></textarea>
+            </span>
         </div>
         <Button/>
-      </div>
+    </div>
     );
   }
